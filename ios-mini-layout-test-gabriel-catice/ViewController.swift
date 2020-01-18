e8dadcbf61ae6063db6d6a41f6e110a3738a94b4
Gabriel Catice <gabriel.catice@icloud.com>
2020-01-17 21:15:58 -0300

    Initial Commit

---
+++ ios-mini-layout-test-gabriel-catice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-mini-layout-test-gabriel-catice
+//
+//  Created by Gabriel on 17/01/20.
+//  Copyright © 2020 GabrielCatice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
